8845c3cb1150d12f68eb5d9b6ef7f802c2058433
Magicxavi <rolfpeterkesche@gmail.com>
2023-09-16 12:24:36 +0300

    socrates: socrates bringup
    * Merge common and device
    * Cleanup
    * Initial blobs
    
    Some changes was taken from https://github.com/kmiit/android_device_xiaomi_socrates
    and from
    https://github.com/lolipuru/device_xiaomi_fuxi
    Co-authored-by: kmiit <2421768138@qq.com>
    Co-authored-by: lolipuru <banstellazz@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The AOSP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_socrates.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_socrates-eng \
+    aosp_socrates-userdebug \
+    aosp_socrates-user
--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+DEVICE=socrates
+VENDOR=xiaomi
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
@@ -24,32 +27,25 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_COMMON=
-ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-common )
-                ONLY_COMMON=true
-                ;;
-        --only-target )
-                ONLY_TARGET=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+    -n | --no-cleanup)
+        CLEAN_VENDOR=false
+        ;;
+    -k | --kang)
+        KANG="--kang"
+        ;;
+    -s | --section)
+        SECTION="${2}"
+        shift
+        CLEAN_VENDOR=false
+        ;;
+    *)
+        SRC="${1}"
+        ;;
     esac
     shift
 done
@@ -60,28 +56,21 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/etc/init/init.embmssl_server.rc)
-            sed -i -n '/interface/!p' "${2}"
+        odm/etc/camera/enhance_motiontuning.xml|odm/etc/camera/enhance_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
             ;;
-        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
-            sed -ni '/dolby/!p' "${2}"
+        odm/etc/camera/motiontuning.xml|odm/etc/camera/motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        odm/etc/camera/night_motiontuning.xml|odm/etc/camera/night_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
             ;;
     esac
 }
 
-if [ -z "${ONLY_TARGET}" ]; then
-    # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
