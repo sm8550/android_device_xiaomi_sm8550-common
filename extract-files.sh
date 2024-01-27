b3498a3afeb2ed8266c835817063e0773bad8211
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-01-27 10:55:27 +0200

    socrates: Patch displayfeature to use v33 libstagefright_foundation
     * this fixes a fatal crash within displayfeature that prevented it from starting
    
    Change-Id: Ic102dc1fb5b27a849b55253d8b81828efd15c1fa

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,9 @@ function blob_fixup() {
         odm/lib64/libxmi_high_dynamic_range_cdsp.so)
             llvm-strip --strip-debug  "${2}"
             ;;
+        odm/lib64/hw/displayfeature.default.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
         odm/etc/init/vendor.xiaomi.sensor.citsensorservice@2.0-service.rc)
             sed -i 's/group system input/group system input\n    task_profiles ServiceCapacityLow/' "${2}"
             ;;
