0cb5df70ca0d1349e143057e50bda2a22a52e228
danielml <daniel@danielml.dev>
2024-07-20 03:18:37 +0300

    socrates: Add MiuiCamera support
    - Add back removed camera libraries
    - Inherit vendor/xiaomi/socrates-camera
    - Label needed postprocessing service
    - Set media profiles xml for enabling supported video modes
    
    Change-Id: Idd3e051f0e5cdd1a06754d91254fd8b05e3fcbb2
    Signed-off-by: danielml <daniel@danielml.dev>

--- vendor-symlinks.mk
+++ vendor-symlinks.mk
@@ -1,36 +1,27 @@
 $(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64/libvndfwk_detect_jni.qti_vendor.so: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/vendor/lib64/libvndfwk_detect_jni.qti_vendor.so,$(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64/libvndfwk_detect_jni.qti_vendor.so)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64/libvndfwk_detect_jni.qti_vendor.so
-$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/vendor/etc/wifi/kiwi/WCNSS_qcom_cfg.ini,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini
-$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/mnt/vendor/persist/kiwi/wlan_mac.bin,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin
+$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,/data/vendor/firmware/wlanmdsp.mbn,$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate
 $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/vendor/etc/wifi/kiwi_v2/WCNSS_qcom_cfg.ini,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini
 $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/mnt/vendor/persist/kiwi_v2/wlan_mac.bin,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi_v2/wlan_mac.bin
+$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,/vendor/etc/wifi/kiwi/WCNSS_qcom_cfg.ini,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/WCNSS_qcom_cfg.ini
+$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,/mnt/vendor/persist/kiwi/wlan_mac.bin,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/kiwi/wlan_mac.bin
 $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/vendor/etc/wifi/qca6490/WCNSS_qcom_cfg.ini,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini
 $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/wlan_mac.bin: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/mnt/vendor/persist/qca6490/wlan_mac.bin,$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/wlan_mac.bin)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/qca6490/wlan_mac.bin
-$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,/data/vendor/firmware/wlanmdsp.mbn,$(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/firmware/wlanmdsp.otaupdate
-$(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,egl/libEGL_adreno.so,$(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
-$(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,egl/libGLESv2_adreno.so,$(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
-$(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so: $(LOCAL_INSTALLED_MODULE)
-$(call symlink-file,,egl/libq3dtools_adreno.so,$(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so
 $(TARGET_OUT_VENDOR)/lib64/camera/aon_front.pb: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/odm/lib64/camera/aon_front.pb,$(TARGET_OUT_VENDOR)/lib64/camera/aon_front.pb)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib64/camera/aon_front.pb
@@ -43,6 +34,15 @@ ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib64/libGLESv2_adreno.so
 $(TARGET_OUT_VENDOR)/lib64/libq3dtools_adreno.so: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,egl/libq3dtools_adreno.so,$(TARGET_OUT_VENDOR)/lib64/libq3dtools_adreno.so)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib64/libq3dtools_adreno.so
+$(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,egl/libEGL_adreno.so,$(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
+$(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,egl/libGLESv2_adreno.so,$(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
+$(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so: $(LOCAL_INSTALLED_MODULE)
+$(call symlink-file,,egl/libq3dtools_adreno.so,$(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/libq3dtools_adreno.so
 $(TARGET_OUT_VENDOR)/rfs/apq/gnss/hlos: $(LOCAL_INSTALLED_MODULE)
 $(call symlink-file,,/mnt/vendor/persist/hlos_rfs/shared,$(TARGET_OUT_VENDOR)/rfs/apq/gnss/hlos)
 ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/rfs/apq/gnss/hlos
